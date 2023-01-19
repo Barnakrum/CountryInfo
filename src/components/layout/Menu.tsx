809586be906f792a0f9bad50afeeac74509186e2
Barnakrum <krzysiu03@tlen.pl>
2023-01-19 17:56:13 +0100

    Initial commit

---
+++ src/components/layout/Menu.tsx
@@ -0,0 +1,8 @@
+export default function Menu() {
+    return (
+        <div>
+            <div className="md:hidden">Mobile menu</div>
+            <div className="hidden md:block">Desktop menu</div>
+        </div>
+    );
+}
---
+++ src/components/layout/Title.tsx
@@ -0,0 +1,3 @@
+export default function Title() {
+    return <div className="text-4xl">Title</div>;
+}
