acc687b267080bfb9d8d3b6974bad609840ebe86
Barnakrum <krzysiu03@tlen.pl>
2023-01-19 18:13:49 +0100

    Added header

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import CountryList from "./components/country/CountryList";
 import Header from "./components/layout/Header";
 
 function App() {
-    return <div className="App w-full"></div>;
+    return (
+        <div className="App w-full">
+            <Header />
+        </div>
+    );
 }
 
 export default App;
