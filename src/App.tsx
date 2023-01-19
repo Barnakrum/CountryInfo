8c258c5ad76d2f215dbd06a91df7d22e61ef1ba2
Barnakrum <krzysiu03@tlen.pl>
2023-01-19 18:13:36 +0100

    Added full width style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import CountryList from "./components/country/CountryList";
+import Header from "./components/layout/Header";
+
 function App() {
-    return <div className="App"></div>;
+    return <div className="App w-full"></div>;
 }
 
 export default App;
