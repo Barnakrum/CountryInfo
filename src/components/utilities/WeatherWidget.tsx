0a64b7500a8e5be0982809b6065b936f440db2a4
Barnakrum <krzysiu03@tlen.pl>
2023-01-20 12:43:23 +0100

    Changed styling and data display

--- src/components/utilities/WeatherWidget.tsx
+++ src/components/utilities/WeatherWidget.tsx
@@ -18,5 +18,24 @@ export default function WeatherWidget(props: { lat: number; lon: number }) {
     if (weatherQuery.isError) {
         return <Error error={weatherQuery.error} />;
     }
-    return <>{weatherQuery.data.timezone}</>;
+    return (
+        <div className="select-none m-0 p-2">
+            <div className="flex flex-col p-2 h-40 w-40 m-4 border-2 border-primary-400 rounded-lg ">
+                <div className="">
+                    <h4>{weatherQuery.data.main.temp}°C</h4>
+                    <div className="group">
+                        <h4 className="">{weatherQuery.data.weather[0].main}</h4>
+                        <div className="origin-left group-hover:scale-100 scale-0 relative left-16 bg-opacity-50 text-opacity-100 bottom-8  text-sm bg-gray-800 p-2 rounded-xl">{weatherQuery.data.weather[0].description}</div>
+                    </div>
+                </div>
+                <img className="p-0 relative bottom-16 -z-10" src={" http://openweathermap.org/img/wn/" + weatherQuery.data.weather[0].icon + "@2x.png"} />
+            </div>
+            <div className="text-xs">
+                Provided by:{" "}
+                <a className="text-primary-400" target={"blank"} href={"https://openweathermap.org"}>
+                    openweathermap.org
+                </a>
+            </div>
+        </div>
+    );
 }
