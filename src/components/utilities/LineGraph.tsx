d1875e86d8a6ee01e7927dbd56aad8b613579f4d
Barnakrum <krzysiu03@tlen.pl>
2023-01-20 19:43:08 +0100

    Initial commit

---
+++ src/components/utilities/LineGraph.tsx
@@ -0,0 +1,28 @@
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Legend } from "chart.js";
+import { Line } from "react-chartjs-2";
+import CountryPopulation from "../../models/UnitedNations/CountryPopulation";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Legend);
+ChartJS.defaults.color = "#ffffff";
+ChartJS.defaults.borderColor = "#gray";
+
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: "top" as const,
+        },
+    },
+};
+
+export default function LineGraph(props: { countryPopulationData: CountryPopulation[] }) {
+    const labels = [...props.countryPopulationData.map((year) => year.timeLabel)];
+
+    return (
+        <>
+            <div className="chart-container w-96 h-96 relative">
+                <Line options={options} data={{ labels: labels, datasets: [{ label: "Population", data: props.countryPopulationData.map((year) => year.value), backgroundColor: "#0d9488", borderColor: "#5eead4" }] }} />
+            </div>
+        </>
+    );
+}
