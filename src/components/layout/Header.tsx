9b5daf531ee2f8a0824abbeacb35d33fd3bea777
Barnakrum <krzysiu03@tlen.pl>
2023-01-20 11:51:57 +0100

    Changed styling

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import Title from "./Title";
 
 export default function Header() {
     return (
-        <div className="h-20 p-4 flex justify-between items-center border-b-2 rounded-xl border-blue-900">
+        <div className="h-20 p-4 flex justify-between items-center border-b-2 border-primary-400">
             <Title />
             <Menu />
         </div>
