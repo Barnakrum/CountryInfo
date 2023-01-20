b6bf854886852d2540f52fc9c23bc3a81d16b00c
Barnakrum <krzysiu03@tlen.pl>
2023-01-20 11:49:24 +0100

    Add route for one country with 3 letter code

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,10 @@
 import CountryList from "./components/country/CountryList";
 import { createBrowserRouter } from "react-router-dom";
+import Country from "./components/country/Country";
 
-const router = createBrowserRouter([{ path: "/", element: <CountryList /> }]);
+const router = createBrowserRouter([
+    { path: "/", element: <CountryList /> },
+    { path: "/country/:countryCode", element: <Country /> },
+]);
 
 export default router;
