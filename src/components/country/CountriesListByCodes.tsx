7426fb2727a9b1f6fb06e7cf4277a78e20a95ab5
Barnakrum <krzysiu03@tlen.pl>
2023-01-22 13:23:26 +0100

    Initial commit

---
+++ src/components/country/CountriesListByCodes.tsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+import { default as CountryModel } from "../../models/Country";
+import Error from "../utilities/Error";
+import Loading from "../utilities/Loading";
+import { BiLinkExternal } from "react-icons/bi";
+
+export default function CountriesListByCodes(props: { codes: string[] }) {
+    const byCodeCountries = useQuery({
+        queryKey: ["country", "code", props.codes],
+        queryFn: async (): Promise<CountryModel[]> => {
+            const apiResponse = await axios.get(import.meta.env.VITE_COUNTRY_API + "/alpha?codes=" + props.codes.join(","));
+            //const apiResponse = await axios.get("https://restcountries.com/v3.1/alpha?codes=can,mex");
+
+            const data = await apiResponse.data;
+            return data;
+        },
+    });
+    if (byCodeCountries.isLoading || byCodeCountries.isIdle) {
+        return <Loading />;
+    }
+    if (byCodeCountries.isError) {
+        return <Error error={byCodeCountries.error} />;
+    }
+    return (
+        <div className="px-4">
+            {byCodeCountries.data.map((country) => {
+                return (
+                    <div>
+                        <a className="flex gap-1" target="_blank" href={"/country/" + country.cca3}>
+                            <span className="flex justify-center items-center">
+                                <img className="w-6" src={country.flags.svg} />
+                            </span>
+                            <h4 className="flex">
+                                {country.name.common}
+                                <span className="pt-1 pl-1 text-primary-400">
+                                    <BiLinkExternal />
+                                </span>
+                            </h4>
+                        </a>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
