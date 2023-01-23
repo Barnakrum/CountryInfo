daa94455c02c81c8ea44d3dc87ec18ee85887dee
Barnakrum <krzysiu03@tlen.pl>
2023-01-23 16:02:05 +0100

    Added path for search

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,12 @@
 import CountryList from "./components/country/CountryList";
 import { createBrowserRouter } from "react-router-dom";
 import Country from "./components/country/Country";
+import Search from "./components/utilities/Search";
 
 const router = createBrowserRouter([
     { path: "/", element: <CountryList /> },
     { path: "/country/:countryCode", element: <Country /> },
+    { path: "/search/:searchQuery", element: <Search /> },
 ]);
 
 export default router;
