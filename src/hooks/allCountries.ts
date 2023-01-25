7cc6fda15c35a37dafc40ec1b038f553085a4c83
Barnakrum <krzysiu03@tlen.pl>
2023-01-25 18:11:23 +0100

    Disabled sorting as this was huge performence killer

--- src/hooks/allCountries.ts
+++ src/hooks/allCountries.ts
@@ -7,7 +7,7 @@ const useAllCountries = () =>
         queryKey: ["allCountries"],
         queryFn: async (): Promise<CountryModel[]> => {
             let data = await (await axios.get(import.meta.env.VITE_COUNTRY_API + "/all")).data;
-            data.sort((a: CountryModel, b: CountryModel) => a.name.common.localeCompare(b.name.common));
+            //data.sort((a: CountryModel, b: CountryModel) => a.name.common.localeCompare(b.name.common));
             return data;
         },
     });
